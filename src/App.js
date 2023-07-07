d809d0630beb3c5f16f4c8d7c563a5de20b25fa4
mauiman77 <boerbasher@protonmail.com>
2023-07-07 13:42:08 +0100

    Add temporary centering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Card from './Card';
 import Head from './Head';
+import './App.css';
 import React, { useState } from 'react';
 
 function App() {
@@ -11,9 +12,22 @@ function App() {
 
 	const [saved, setSaved] = useState([])
 
+	const resetScore = () => {
+		setState(prev => {
+			return {
+				...prev,
+				score: 0
+			}
+		})
+	}
+
+	const mockValues = ['red', 'orange', 'yellow', 'green']
+
 	const handleLogic = (cardName) => {
 		if (saved.includes(cardName)) {
-			console.log('HH')
+			resetScore();
+		} else {
+			handleScore();
 		}
 		setSaved(prev => prev.concat(cardName))
 		console.log(saved)
@@ -34,11 +48,13 @@ function App() {
 			<Head
 			title="score"
 			score={state.score} />
-			<div>
-				<Card name='bad' handleLogic={handleLogic} />
-				<Card name='Ok' handleLogic={handleLogic} />
-				<Card name='Awesome' handleLogic={handleLogic} />
-				<Card name='Epic' handleLogic={handleLogic} />
+			<div className="game-container">
+				<div className="card-container">
+					<Card name={mockValues[0]} handleLogic={handleLogic} />
+					<Card name='Ok' handleLogic={handleLogic} />
+					<Card name='Awesome' handleLogic={handleLogic} />
+					<Card name='Epic' handleLogic={handleLogic} />
+				</div>
 			</div>
 		</div>
 	)
